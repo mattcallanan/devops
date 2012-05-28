913ca0b9e9e8db93fa55ad022225f5f82596ae94
Matt Callanan <matthew_callanan@yahoo.com.au>
2012-05-29 02:17:52 +1000

    Mind maps

---
+++ Cloud.html_files/map.mm
@@ -0,0 +1,20 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1334321151941" ID="ID_364753400" LINK="DevOpsResourcesMindMap.mm" MODIFIED="1338221454268" TEXT="Cloud">
+<node CREATED="1334321156600" ID="ID_563582240" MODIFIED="1334321162259" POSITION="left" TEXT="CloudFoundry BOSH">
+<node CREATED="1334321163701" MODIFIED="1334321168768" TEXT="Deploy to multiple clouds"/>
+<node CREATED="1334321170460" MODIFIED="1334321180250" TEXT="Avoid cloud lock-in and downtime impacts"/>
+<node CREATED="1334321226401" LINK="https://github.com/cloudfoundry/oss-docs/blob/master/bosh/documentation/documentation.md" MODIFIED="1334321236009" TEXT="Github"/>
+</node>
+<node CREATED="1334321298579" ID="ID_246421856" MODIFIED="1334321301729" POSITION="left" TEXT="Cloud9 IDE">
+<node CREATED="1334321302899" MODIFIED="1334321312102" TEXT="Develop and deploy in the cloud (via browser)"/>
+<node CREATED="1334321312919" MODIFIED="1334321323617" TEXT="Heroku, CloudFoundry, Joyent"/>
+</node>
+<node CREATED="1334323756570" ID="ID_1119451270" MODIFIED="1334323758575" POSITION="right" TEXT="Providers">
+<node CREATED="1334323760104" MODIFIED="1334323761716" TEXT="Joyent"/>
+<node CREATED="1334323762544" MODIFIED="1334323765602" TEXT="Amazon"/>
+<node CREATED="1334323766159" MODIFIED="1334323770778" TEXT="Microsoft Azure"/>
+<node CREATED="1334323771300" MODIFIED="1334323777914" TEXT="CouldFoundry"/>
+</node>
+</node>
+</map>
